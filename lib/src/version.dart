45035d4d98445cbe70f10e227a14c87822995e03
raulmabe <raulmateob@gmail.com>
2022-06-28 18:03:42 +0200

    fix: version file wasn't being generated

--- lib/src/version.dart
+++ lib/src/version.dart
@@ -1,9 +1,2 @@
-// Copyright (c) 2022, Raul Mateo Beneyto
-// https://raulmabe.dev
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
-
 // Generated code. Do not modify.
-const packageVersion = '0.0.1';
+const packageVersion = '0.0.4';
